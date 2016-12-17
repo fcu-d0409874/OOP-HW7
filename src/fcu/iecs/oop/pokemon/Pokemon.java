d995aa9ac1e6b19637dbc2802e78e3dcacd415d6
fcu-d0409874 <jim23693728@gmail.com>
2016-12-17 18:59:48 +0800

    HW7

---
+++ src/fcu/iecs/oop/pokemon/Pokemon.java
@@ -0,0 +1,35 @@
+package fcu.iecs.oop.pokemon;
+
+public abstract class Pokemon {
+
+	private String name;
+
+	private PokemonType type;
+
+	private int cp;
+
+	public Pokemon(String name, PokemonType type, int cp) {
+		this.name = name;
+		this.cp = cp;
+		this.type = type;
+	}
+
+	public int getCp() {
+		return cp;
+	}
+
+	public void setCp(int cp) {
+		this.cp = cp;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public PokemonType getType() {
+		return type;
+	}
+
+	public abstract void attack();
+
+}
